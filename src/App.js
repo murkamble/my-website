941fa3c4fda44e3217b5df82604ecb4d293ba21c
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-06-01 15:23:28 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react"
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <table>
+      <tr>
+        <td>Name</td>
+        <td>:</td>
+        <td>Mayur Kamble</td>
+        </tr>
+        <tr>
+        <td>Email</td>
+        <td>:</td>
+        <td>cse.mkamble@gmail.com</td>
+        </tr>
+        <tr>
+        <td>Phone</td>
+        <td>:</td>
+        <td>+91 951194513</td>
+        </tr>
+        <tr>
+        <td>Role</td>
+        <td>:</td>
+        <td>Full Stack Developer</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default App
